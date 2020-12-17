d7b155f4682564e77ab39c9bb27fb0bd4d44db1c
NicolaDonoastro <nicolaardiansya@gmail.com>
2020-12-17 15:06:54 +0700

    add size filter and file filter

--- src/middleware/upload.js
+++ src/middleware/upload.js
@@ -2,6 +2,16 @@ const multer = require('multer')
 
 const upload = multer({
   dest: 'avatar',
+  limits: {
+    fileSize: 1000000,
+  },
+  fileFilter(req, file, cb) {
+    if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
+      return cb(new Error('Please upload a image file'))
+    }
+
+    cb(undefined, true)
+  },
 })
 
 module.exports = upload
